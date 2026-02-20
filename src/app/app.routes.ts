b0dbc92931da72c6f650cd0b107cacae01ba5353
Julien MARTIN <ju.martin@outlook.com>
2026-02-20 14:17:58 +0100

    redesign fiche & fiche form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import {Home} from './pages/home/home';
 import {SingleTrail} from './pages/single-trail/single-trail';
 import {MesSentiers} from './pages/mes-sentiers/mes-sentiers';
 import {authGuard} from './core/auth/auth-guard';
-import {Fiche} from './pages/fiche/fiche';
+import {FichePage} from './pages/fiche/fiche';
 
 export const routes: Routes = [
   {path: '', component: Home, title: "Smart'Flore"},
   {path: 'me', component: MesSentiers, canActivate: [authGuard], title: 'Mes sentiers - Smart\'Flore'},
   {path: 'trail/:id', component: SingleTrail, title: 'sentier - Smart\'Flore'},
-  {path: 'fiche/:referentiel/:num_taxonomic/:num_nom', component: Fiche, title: "Fiche Smart'Flore"},
+  {path: 'fiche/:referentiel/:num_taxonomic/:num_nom', component: FichePage, title: "Fiche Smart'Flore"},
   // {path: 'lazy', loadChildren: () => import('./features/sentier/components/single-trail/single-trail.module').then(m => m.SingleTrailModule)},
   { path: 'not-found', component: Home , title: "Smart'Flore"},
   { path: '**', redirectTo: 'not-found'}
--- src/app/pages/fiche/fiche.ts
+++ src/app/pages/fiche/fiche.ts
@@ -1,9 +1,14 @@
-import {ChangeDetectionStrategy, Component, inject, input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, input, OnInit, signal} from '@angular/core';
 import {TaxonSearchService} from '../../features/taxon/services/taxon-search-service';
 import {Loader} from '../../shared/components/loader/loader';
 import {WikiToHtmlPipe} from '../../features/fiche/pipes/WikiToHtmlPipe';
 import {MarkdownPipe} from '../../features/fiche/pipes/MarkdownPipe';
 import {ErrorComponent} from '../../shared/components/error/error';
+import {SharedService} from '../../shared/services/shared.service';
+import {FicheService} from '../../features/fiche/services/fiche-service';
+import {Fiche} from '../../features/fiche/models/fiche.model';
+import {FicheForm} from '../../features/fiche/components/fiche-form/fiche-form';
+import {UserService} from '../../core/auth/services/user.service';
 
 @Component({
   selector: 'app-fiche',
@@ -11,19 +16,43 @@ import {ErrorComponent} from '../../shared/components/error/error';
     Loader,
     WikiToHtmlPipe,
     MarkdownPipe,
-    ErrorComponent
+    ErrorComponent,
+    FicheForm
   ],
   templateUrl: './fiche.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Fiche implements OnInit {
+export class FichePage implements OnInit {
   readonly referentiel = input.required<string>()
   readonly num_taxonomic = input.required<number>()
   readonly num_nom = input.required<number>()
 
+  readonly showFicheModal = signal(false);
+  readonly fiche = signal<Fiche | null>(null);
+
   taxonSearchService = inject(TaxonSearchService);
+  sharedService = inject(SharedService);
+  ficheService = inject(FicheService);
+  userService = inject(UserService);
 
   ngOnInit(): void {
     this.taxonSearchService.getTaxonFiche(this.referentiel(), this.num_nom())
+  }
+
+  async editFicheModal(): Promise<void> {
+    this.fiche.set(null);
+    await this.ficheService.fetchFiche(
+      this.referentiel(),
+      this.num_nom()
+    )
+    this.fiche.set(this.ficheService.fiche());
+    this.showFicheModal.set(true);
+    this.sharedService.toggleBlurBackground()
+  }
+
+  closeFicheModal(): void {
+    this.fiche.set(null);
+    this.showFicheModal.set(false);
+    this.sharedService.toggleBlurBackground()
   }
 }
